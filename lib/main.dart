4852873d57ca2bacfb559f68c961c7659fba4553
ydmins <dr.ydmins@gmail.com>
2025-05-18 16:14:24 +0900

    feature: configure the routing structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/di/di_helper.dart';
+import 'core/router/app_router.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
+  runApp(const BudgetApp());
+}
+
+class BudgetApp extends StatelessWidget {
+  const BudgetApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _router = AppRouter().router;
+
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'Budget App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
